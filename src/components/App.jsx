c28b729455a8759d5db0cfa820b7da0464583f06
Your Name <you@example.com>
2020-06-15 19:13:38 +0300

    feat(envs): add configs for dev mode and prod mode; Because task is simple i add one config file wich depends on env variable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>My React App!!!</h1>
-
+        <h1>My React App!</h1>
         <div className="assets">
           <img src={Jpg} />
           <img src={Svg} />
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,48 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.jsx",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: "file-loader",
+module.exports = (env) => {
+  const optimization = env.match(/dev/gi)
+    ? {
+        minimize: false,
+      }
+    : {
+        minimize: true,
+      };
+  return {
+    entry: "./src/index.jsx",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    optimization,
+    module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
           },
-        ],
-      },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  };
 };
