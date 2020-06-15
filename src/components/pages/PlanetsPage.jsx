61c3ad4f18ad88629f008818ca201f6f875b7ea9
Your Name <you@example.com>
2020-06-15 20:13:44 +0300

    feat(init)

---
+++ src/components/pages/PlanetsPage.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export const PlanetsPage = () => {
+  return <div>PlanetsPage</div>;
+};
---
+++ src/components/pages/StarshipsPage.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export const StarshipsPage = () => {
+  return <div>StarshipsPage</div>;
+};
